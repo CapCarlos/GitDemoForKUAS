44e67bd4e917b67813946007607123bbc4a3c1b4
ting_yu <afa60041@gmail.com>
2016-03-03 15:02:38 +0800

    test1

---
+++ WebApplication1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Class1
+    {
+        public static void Main() {
+
+            System.Console.WriteLine("hi");
+            System.Console.ReadLine();
+
+        }
+
+    }
+}
