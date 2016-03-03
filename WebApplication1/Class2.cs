29ecefcf6638da7bf10c6188e71290f364461863
1102137216 <1102137216@gm.kuas.edu.tw>
2016-03-03 15:42:35 +0800

    hmkhgh

---
+++ WebApplication1/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Class2
+    {
+        System.Console.WriteLine("Hello~~~");
+            System.Console.ReadLine();
+            System.Console.ReadLine();
+    }
+}
